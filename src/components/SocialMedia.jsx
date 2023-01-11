feaeb3a45c540644165df01cb8e93609a5047bbc
Nawaz Danish <contact.to.nd@gmail.com>
2023-01-12 00:54:01 +0530

    updated

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,8 +4,11 @@ import "../css/SocialMedia.css";
 import fbImg from "../img/socialMediaIcons/facebook.png";
 import waImg from "../img/socialMediaIcons/whatsapp.png";
 import ytImg from "../img/socialMediaIcons/youtube.png";
+import useSound from 'use-sound';
+import boopSfx from "../sound/socialMediaSound.mp3";
 
 const SocialMedia = () => {
+  const [play, {stop}] = useSound(boopSfx);
 
   useEffect(()=>{
     Aos.init({duration: 2000});
@@ -14,13 +17,13 @@ const SocialMedia = () => {
   return (
     <div id='socialMedida' data-aos="fade-left">
         <a href="https://www.facebook.com/GoalpokherAlMadadWelfareSociety" target="blank">
-            <img src={fbImg} loading="lazy" alt="Facebook" />
+            <img onMouseEnter={()=> play()} src={fbImg} loading="lazy" alt="Facebook" />
         </a>
         <a href="https://chat.whatsapp.com/FDh9eTo0dyF1YPgXRHQ1hb" target="blank">
-            <img src={waImg} loading="lazy" alt="What's App" />
+            <img onMouseEnter={()=> play()} src={waImg} loading="lazy" alt="What's App" />
         </a>
         <a href="true" target="blank">
-            <img src={ytImg} loading="lazy" alt="YouTube" />
+            <img onMouseEnter={()=>play()} onMouseLeave={()=>stop()} src={ytImg} loading="lazy" alt="YouTube" />
         </a>
     </div>
   )
