3d5dee2deb4bd21739682e49ee54cd4905bfdc5a
Nawaz Danish <contact.to.nd@gmail.com>
2023-01-16 11:20:13 +0530

    updated

--- src/components/CounterUp.jsx
+++ src/components/CounterUp.jsx
@@ -1,39 +1,79 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "../css/CounterUp.css";
-import { CountUp } from 'use-count-up';
+import Aos from 'aos';
+import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
 
 const CounterUp = () => {
+    const [counterOn, setCounterOn] = useState(false);
+
+    useEffect(()=>{
+        Aos.init({duration: 2000});
+    },[])
+
   return (
     <div className='bg-dark'>
+        <ScrollTrigger 
+            onEnter={()=>setCounterOn(true)}
+            onExit={()=>setCounterOn(false)}
+        >
         <div className="container">
             <div className="row">
 
                 <div className="col-md-4 p-5 mi">
-                    <h1 className='text-warning'>
-                        <CountUp isCounting end={150} duration={3.2} />
-                        <span>+</span>
-                    </h1>
+                        <h1 className='text-warning'>
+                            {
+                            counterOn &&
+                            <CountUp 
+                                isCounting
+                                start={0} 
+                                end={150}
+                                delay={0} 
+                                duration={5}
+                                />
+                            }
+                            <span>+</span>
+                        </h1>
                     <h6 style={{color: "#7f8c8d"}}>Volunteers Engaged this Year</h6>
                 </div>
 
                 <div className="col-md-4 p-5 mi">
-                    <h1 className='text-warning'>
-                        <CountUp isCounting end={4} duration={3.2} />
-                        <span>+</span>
-                    </h1>
+                        <h1 className='text-warning'>
+                            {
+                                counterOn &&
+                                <CountUp 
+                                    isCounting
+                                    start={0} 
+                                    end={4} 
+                                    delay={0}
+                                    duration={3} 
+                                />
+                            }
+                            <span>+</span>
+                        </h1>
                     <h6 style={{color: "#7f8c8d"}}>Years of Voluntary Service</h6>
                 </div>
 
                 <div className="col-md-4 p-5 mi">
-                    <h1 className='text-warning'>
-                        <CountUp isCounting end={527} duration={3.2} />
-                        <span>+</span>
-                    </h1>
+                        <h1 className='text-warning'>
+                            {
+                                counterOn &&
+                                <CountUp 
+                                    isCounting
+                                    start={0}
+                                    end={527}
+                                    delay={0} 
+                                    duration={3}
+                                />
+                            }
+                            <span>+</span>
+                        </h1>
                     <h6 style={{color: "#7f8c8d"}}>Volunteering Hours this Year</h6>
                 </div>
 
             </div>
         </div>
+        </ScrollTrigger>
     </div>
   )
 }
